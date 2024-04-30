d94ae2bab2024f9650efdd497cf751787b4bd219
Raul Sanchez R. <raul.sr92@gmail.com>
2024-04-29 23:39:05 -0500

    Creacion de array de objetos conteniendo a los productos de la tienda

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+// Crear objeto de productos de la tienda
+
+const productos =[
+    {
+        producto: "Camiseta Away Mujer 2024",
+        precio: 249.9,
+        imagen: ""
+    },
+
+    {
+        producto: "Camiseta Home Adulto 2024 Con Sponsors",
+        precio: 269.9,
+        imagen: ""
+    },
+
+    {
+        producto: "Camiseta Away Adulto 2024 Con Sponsors",
+        precio: 269.9,
+        imagen: ""
+    },
+
+    {
+        producto: "Gorra Alianza Lima Nike 2024",
+        precio: 89.9,
+        imagen: ""
+    },
+    {
+        producto: "Camiseta Alterna Adulto 2023 Sin Sponsors + Parche Campeón",
+        precio: 219.9,
+        imagen: ""
+    },
+    {
+        producto: "Camiseta Alterna Adulto 2023 Con Sponsors + Parche Campeón",
+        precio: 299.9,
+        imagen: ""
+    },
+    {
+        producto: "Polo de Algodón Azul Alianza Lima",
+        precio: 99.9,
+        imagen: ""
+    },
+    {
+        producto: "Polo Nike Azul de concentración",
+        precio: 159.0,
+        imagen: ""
+    },
+    {
+        producto: "Bandera Oficial Club Alianza Lima",
+        precio: 39.90,
+        imagen: ""
+    },
+    {
+        producto: "Banderín oficial - Club Alianza Lima",
+        precio: 99.90,
+        imagen: ""
+    },
+    {
+        producto: "Rompecabezas Señor de los Milagros",
+        precio: 79.90,
+        imagen: ""
+    }
+    ,
+    {
+        producto: "Audifonos Sudio True Wireless Edición Alianza Lima",
+        precio: 169.90,
+        imagen: ""
+    }
+]
